e6eadd02c18eeef554e11486c03e3369a22acd70
Adityasingh675 <adisingh675@gmail.com>
2020-12-18 03:22:01 +0530

    device real test

--- djstackflow/djstackflow/settings.py
+++ djstackflow/djstackflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')2dh!hgf+%2fdd9vznj0tl04_dh8u=fr!b=ru_kq)-qiz@t=+x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.179']
 
 
 # Application definition
